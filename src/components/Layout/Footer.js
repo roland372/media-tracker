1b15f08479edc005afd4633f2b3fc37628460cc7
roland372 <damfat94@gmail.com>
2022-06-21 19:28:18 +0200

    added footer text

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -5,7 +5,9 @@ const Footer = () => {
 		<div className='d-flex flex-column border-top'>
 			<footer className='footer mt-auto py-3 bg-primary-dark'>
 				<div className='container'>
-					<div className='text-color mx-2'>Footer text</div>
+					<div className='text-color mx-2'>
+						<p>© {new Date().getFullYear()} Media-Tracker</p>
+					</div>
 				</div>
 			</footer>
 		</div>
