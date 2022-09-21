c6bd00e1aa4419f709331d37ed6000ca0873c9fd
roland372 <damfat94@gmail.com>
2022-09-21 22:45:10 +0200

    added searching and filtering functionality

--- src/components/Notes/pages/NotesList.js
+++ src/components/Notes/pages/NotesList.js
@@ -36,10 +36,13 @@ const NotesList = () => {
 
 	//* <----- Modal state ----->
 	const [show, setShow] = useState(false);
+	const [showSettingsModal, setShowSettingsModal] = useState(false);
 
 	//* <----- Modal functions ----->
-	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
+	const handleClose = () => setShow(false);
+	const handleShowSettings = () => setShowSettingsModal(true);
+	const handleCloseSettings = () => setShowSettingsModal(false);
 
 	//* Notifications
 	const NoteDeletedNotification = () =>
@@ -87,92 +90,287 @@ const NotesList = () => {
 	// 	return tmp.textContent || tmp.innerText || '';
 	// }
 
+	//* sorting
 	const notesCopy = [...notes?.notes];
 	const sortByDate = notesCopy?.sort((a, b) => b.lastModified - a.lastModified);
-	// console.log(sortByDate?.[0]?.note);
+
+	const [sortedNotes, setSortedNotes] = useState(sortByDate);
+	const [order, setOrder] = useState('DSC');
+
+	const sortNotes = (order, menuItems, column) => {
+		if (order === 'ASC') {
+			const sorted = [...menuItems].sort((a, b) =>
+				a[column] > b[column] ? 1 : -1
+			);
+			// return sorted;
+			setSortedNotes(sorted);
+			setOrder('DSC');
+		}
+		if (order === 'DSC') {
+			const sorted = [...menuItems].sort((a, b) =>
+				a[column] < b[column] ? 1 : -1
+			);
+			// return sorted;
+			setSortedNotes(sorted);
+			setOrder('ASC');
+		}
+	};
+
+	const handleSortByTitle = () => {
+		sortNotes(order, sortByDate, 'title');
+	};
+	const handleSortByColor = () => {
+		sortNotes(order, sortByDate, 'color');
+	};
+	const handleSortByLastModified = () => {
+		sortNotes(order, sortByDate, 'lastModified');
+	};
+
+	//* searching
+	const [searchTerm, setSearchTerm] = useState('');
 
 	const renderNotes = () =>
-		sortByDate?.map(note => (
-			<div
-				className='col-lg-4 col-sm-6 col-12 my-2 text-wrap flex-fill'
-				key={note.id}
-				style={{ wordWrap: 'break-word' }}
-			>
+		sortedNotes
+			?.filter(value => {
+				if (searchTerm === '') {
+					return value;
+				} else if (
+					value.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+				) {
+					return value;
+				}
+				return 0;
+			})
+			.map(note => (
 				<div
-					className={`border-top border-${note?.color} border-5 rounded bg-primary-dark p-3 text-wrap h-100 d-flex flex-column justify-content-between shadow-lg `}
+					className='col-lg-4 col-sm-6 col-12 my-2 text-wrap flex-fill'
+					key={note.id}
+					style={{ wordWrap: 'break-word' }}
 				>
-					<div className='text-start'>
-						<h3>
-							{note.title.length > 30
-								? note.title.slice(0, 30) + '...'
-								: note.title}
-						</h3>
-						<hr />
-						{note.note.length > 200 ? (
-							<div
-								dangerouslySetInnerHTML={{
-									__html:
-										note.note
-											.slice(0, 200)
-											.replace(
-												/<img[^>]+>(<\/img>)?|<iframe.+?<\/iframe>/g,
-												''
-											) + '...',
-								}}
-							/>
-						) : (
-							<div
-								dangerouslySetInnerHTML={{
-									__html: note.note.replace(
-										/<img[^>]+>(<\/img>)?|<iframe.+?<\/iframe>/g,
-										''
-									),
-								}}
-							/>
-						)}
-					</div>
-					<section className='d-flex justify-content-between align-items-center'>
-						<div className='text-muted'>
-							{new Date(note.lastModified).toLocaleDateString('en-GB', {
-								hour: '2-digit',
-								minute: '2-digit',
-							})}
+					<div
+						className={`border-top border-${note?.color} border-5 rounded bg-primary-dark p-3 text-wrap h-100 d-flex flex-column justify-content-between shadow-lg `}
+					>
+						<div className='text-start'>
+							<h3>
+								{note.title.length > 30
+									? note.title.slice(0, 30) + '...'
+									: note.title}
+							</h3>
+							<hr />
+							{note.note.length > 200 ? (
+								<div
+									dangerouslySetInnerHTML={{
+										__html:
+											note.note
+												.slice(0, 200)
+												.replace(
+													/<img[^>]+>(<\/img>)?|<iframe.+?<\/iframe>/g,
+													''
+												) + '...',
+									}}
+								/>
+							) : (
+								<div
+									dangerouslySetInnerHTML={{
+										__html: note.note.replace(
+											/<img[^>]+>(<\/img>)?|<iframe.+?<\/iframe>/g,
+											''
+										),
+									}}
+								/>
+							)}
 						</div>
-						<div className=''>
-							<Button
-								sm
-								text={
-									<Link to={`${note.noteID}`}>
-										<BiLinkExternal size={20} className='text-primary' />
-									</Link>
-								}
-							/>
-							<Button
-								sm
-								text={
-									<Link to={`edit-note/${note.noteID}`}>
-										<AiOutlineEdit size={20} className='text-success' />
-									</Link>
-								}
-							/>
+						<section className='d-flex justify-content-between align-items-center'>
+							<div className='text-muted'>
+								{new Date(note.lastModified).toLocaleDateString('en-GB', {
+									hour: '2-digit',
+									minute: '2-digit',
+								})}
+							</div>
+							<div className=''>
+								<Button
+									sm
+									text={
+										<Link to={`${note.noteID}`}>
+											<BiLinkExternal size={20} className='text-primary' />
+										</Link>
+									}
+								/>
+								<Button
+									sm
+									text={
+										<Link to={`edit-note/${note.noteID}`}>
+											<AiOutlineEdit size={20} className='text-success' />
+										</Link>
+									}
+								/>
 
-							<Button
-								// onClick={() => handleDeleteNote(note.noteID)}
-								onClick={() => {
-									setNoteID(note.noteID);
-									handleShow();
-								}}
-								sm
-								text={<BsTrash size={20} className='text-danger' />}
-							/>
-						</div>
-					</section>
+								<Button
+									// onClick={() => handleDeleteNote(note.noteID)}
+									onClick={() => {
+										setNoteID(note.noteID);
+										handleShow();
+									}}
+									sm
+									text={<BsTrash size={20} className='text-danger' />}
+								/>
+							</div>
+						</section>
+					</div>
 				</div>
-			</div>
-		));
+			));
+
+	const renderNotes2 = () =>
+		sortByDate
+			?.filter(value => {
+				if (searchTerm === '') {
+					return value;
+				} else if (
+					value.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+				) {
+					return value;
+				}
+				return 0;
+			})
+			.map(note => (
+				<div
+					className='col-lg-4 col-sm-6 col-12 my-2 text-wrap flex-fill'
+					key={note.id}
+					style={{ wordWrap: 'break-word' }}
+				>
+					<div
+						className={`border-top border-${note?.color} border-5 rounded bg-primary-dark p-3 text-wrap h-100 d-flex flex-column justify-content-between shadow-lg `}
+					>
+						<div className='text-start'>
+							<h3>
+								{note.title.length > 30
+									? note.title.slice(0, 30) + '...'
+									: note.title}
+							</h3>
+							<hr />
+							{note.note.length > 200 ? (
+								<div
+									dangerouslySetInnerHTML={{
+										__html:
+											note.note
+												.slice(0, 200)
+												.replace(
+													/<img[^>]+>(<\/img>)?|<iframe.+?<\/iframe>/g,
+													''
+												) + '...',
+									}}
+								/>
+							) : (
+								<div
+									dangerouslySetInnerHTML={{
+										__html: note.note.replace(
+											/<img[^>]+>(<\/img>)?|<iframe.+?<\/iframe>/g,
+											''
+										),
+									}}
+								/>
+							)}
+						</div>
+						<section className='d-flex justify-content-between align-items-center'>
+							<div className='text-muted'>
+								{new Date(note.lastModified).toLocaleDateString('en-GB', {
+									hour: '2-digit',
+									minute: '2-digit',
+								})}
+							</div>
+							<div className=''>
+								<Button
+									sm
+									text={
+										<Link to={`${note.noteID}`}>
+											<BiLinkExternal size={20} className='text-primary' />
+										</Link>
+									}
+								/>
+								<Button
+									sm
+									text={
+										<Link to={`edit-note/${note.noteID}`}>
+											<AiOutlineEdit size={20} className='text-success' />
+										</Link>
+									}
+								/>
+
+								<Button
+									// onClick={() => handleDeleteNote(note.noteID)}
+									onClick={() => {
+										setNoteID(note.noteID);
+										handleShow();
+									}}
+									sm
+									text={<BsTrash size={20} className='text-danger' />}
+								/>
+							</div>
+						</section>
+					</div>
+				</div>
+			));
 
 	return (
 		<CardComponent title='Notes'>
+			<Modal show={showSettingsModal} onHide={handleCloseSettings}>
+				<Modal.Header
+					closeButton
+					closeVariant='white'
+					className='bg-primary-light text-color'
+				>
+					<Modal.Title>Settings</Modal.Title>
+				</Modal.Header>
+				<Modal.Body className='bg-primary-dark text-color'>
+					<div>
+						<label className='form-label'>Search</label>
+						<input
+							type='text'
+							className='form-control mb-2'
+							placeholder='Search for a note'
+							onChange={e => {
+								setSearchTerm(e.target.value);
+							}}
+						/>
+					</div>
+					<div>
+						<label className='form-label'>Sort by</label>
+						<div className='d-flex justify-content-start'>
+							<Button
+								color='primary'
+								onClick={() => handleSortByTitle()}
+								sm
+								text='Title'
+							/>
+							<div className='mx-2'>
+								<Button
+									color='success'
+									onClick={() => handleSortByColor()}
+									sm
+									text='Color'
+								/>
+							</div>
+							<Button
+								color='warning'
+								onClick={() => handleSortByLastModified()}
+								sm
+								text='Last modified'
+							/>
+						</div>
+					</div>
+				</Modal.Body>
+				{/* <Modal.Footer className='bg-primary-dark text-color'>
+					<Button color='warning' onClick={handleClose} text='Cancel' />
+					<Button
+						color='danger'
+						onClick={() => {
+							handleDeleteNote(noteID);
+							handleClose();
+						}}
+						text='Delete'
+					/>
+				</Modal.Footer> */}
+			</Modal>
 			<Modal show={show} onHide={handleClose}>
 				<Modal.Header
 					closeButton
@@ -197,10 +395,15 @@ const NotesList = () => {
 				</Modal.Footer>
 			</Modal>
 			<section className='text-color'>
-				<div className='d-flex align-items-center justify-content-start mx-2 pt-1'>
+				<div className='d-flex align-items-center justify-content-between mx-2 pt-1'>
 					<Link to='/notes/add-note'>
 						<Button color='primary' text='Add Note' />
 					</Link>
+					<Button
+						color='warning'
+						onClick={() => handleShowSettings()}
+						text='Settings'
+					/>
 				</div>
 				{/* <button className='btn btn-primary' onClick={getNotes}>
 					Fetch Notes
@@ -213,7 +416,17 @@ const NotesList = () => {
 				<Loader />
 			) : (
 				<div className='row px-2'>
-					{notes.notes.length ? renderNotes() : <p>No Notes</p>}
+					{/* {notes?.notes?.length ? renderNotes() : <p>No Notes</p>} */}
+					{notes?.notes?.length ? (
+						sortedNotes?.length ? (
+							renderNotes()
+						) : (
+							renderNotes2()
+						)
+					) : (
+						<Loader />
+					)}
+					{/* {sortedNotes?.length ? renderNotes() : renderNotes2()} */}
 				</div>
 			)}
 		</CardComponent>
